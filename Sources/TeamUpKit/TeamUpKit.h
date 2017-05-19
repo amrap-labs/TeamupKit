ca929c843ce56e5ce1b46475c980852ae3594654
Merrick Sapsford <merricks@apadmi.com>
2017-05-19 18:01:44 +0100

    Add project

---
+++ Sources/TeamUpKit/TeamUpKit.h
@@ -0,0 +1,19 @@
+//
+//  TeamUpKit.h
+//  TeamUpKit
+//
+//  Created by Merrick Sapsford on 19/05/2017.
+//  Copyright © 2017 Merrick Sapsford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TeamUpKit.
+FOUNDATION_EXPORT double TeamUpKitVersionNumber;
+
+//! Project version string for TeamUpKit.
+FOUNDATION_EXPORT const unsigned char TeamUpKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TeamUpKit/PublicHeader.h>
+
+
