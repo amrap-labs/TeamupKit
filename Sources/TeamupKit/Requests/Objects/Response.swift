634fd353526951fa26d4f30b30e4ac1e0db3bfbb
Merrick Sapsford <merrick@sapsford.tech>
2017-07-26 10:18:21 +0100

    Add error detail to response

--- Sources/TeamupKit/Requests/Objects/Response.swift
+++ Sources/TeamupKit/Requests/Objects/Response.swift
@@ -24,6 +24,8 @@ public class Response {
     let data: Data?
     /// The error included with the response.
     let error: Error?
+    /// Details associated with an erronous response.
+    private(set) var errorDetail: ErrorDetail?
     
     /// Whether the response is a successful one.
     var isSuccessful: Bool {
@@ -38,6 +40,14 @@ public class Response {
         self.statusCode = statusCode
         self.data = data
         self.error = error
+        
+        /// Parse error details
+        if let data = data, error != nil {
+            let decoder = JSONDecoder()
+            do {
+                self.errorDetail = try decoder.decode(ErrorDetail.self, from: data)
+            } catch {}
+        }
     }
 }
 
