a25deea48137a1cf7bdc7d5b20c2536c1bff8f1d
Merrick Sapsford <merrick@sapsford.tech>
2017-07-26 11:54:39 +0100

    Fix memberships requests

--- Sources/TeamupKit/Controllers/Account/Memberships/MembershipsApiController.swift
+++ Sources/TeamupKit/Controllers/Account/Memberships/MembershipsApiController.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Alamofire
 
 class MembershipsApiController: AuthenticatedController, MembershipsController {
     
@@ -15,56 +16,57 @@ class MembershipsApiController: AuthenticatedController, MembershipsController {
     func loadAll(success: ((ResultsPage<Membership>) -> Void)?,
                  failure: Controller.MethodFailure?) {
         
-//        var parameters = Request.Parameters()
-//        parameters.set(auth?.currentUser?.customer.id, for: "customer")
-//        parameters.set(config.business.businessId, for: "business")
-//
-//        let request = requestBuilder.build(for: .memberships,
-//                                           method: .get,
-//                                           contentType: .json,
-//                                           parameters: parameters,
-//                                           authentication: .userToken)
-//        requestExecutor.execute(request: request,
-//                                success:
-//            { (request, response, data) in
-//                guard let data = data else {
-//                    failure?(RequestError.unknown)
-//                    return
-//                }
-//                do {
-//                    let memberships = try self.decoder.decode(ResultsPage<Membership>.self, from: data)
-//                    success?(memberships)
-//                } catch {
-//                    failure?(RequestError(with: error))
-//                }
-//        }) { (request, response, error) in
-//            failure?(error)
-//        }
+        var parameters: Alamofire.Parameters = [
+            "business" : config.business.businessId,
+        ]
+        if let customerId = auth?.currentUser?.customer.id {
+            parameters["customer"] = customerId
+        }
+        
+        let request = requestBuilder.build(for: .memberships,
+                                           method: .get,
+                                           parameters: parameters,
+                                           authentication: .userToken)
+        requestExecutor.execute(request: request,
+                                success:
+            { (request, response, data) in
+                guard let data = data else {
+                    failure?(TeamupError.unknown, nil)
+                    return
+                }
+                do {
+                    let memberships = try self.decoder.decode(ResultsPage<Membership>.self, from: data)
+                    success?(memberships)
+                } catch {
+                    failure?(error, nil)
+                }
+        }) { (request, response, error) in
+            failure?(error, response?.errorDetail)
+        }
     }
     
     func load(withId id: Int,
               success: ((Membership) -> Void)?,
               failure: Controller.MethodFailure?) {
         
-//        let request = requestBuilder.build(for: .membership(id: id),
-//                                           method: .get,
-//                                           contentType: .json,
-//                                           authentication: .userToken)
-//        requestExecutor.execute(request: request,
-//                                success:
-//            { (request, response, data) in
-//                guard let data = data else {
-//                    failure?(RequestError.unknown)
-//                    return
-//                }
-//                do {
-//                    let membership = try self.decoder.decode(Membership.self, from: data)
-//                    success?(membership)
-//                } catch {
-//                    failure?(RequestError(with: error))
-//                }
-//        }) { (request, response, error) in
-//            failure?(error)
-//        }
+        let request = requestBuilder.build(for: .membership(id: id),
+                                           method: .get,
+                                           authentication: .userToken)
+        requestExecutor.execute(request: request,
+                                success:
+            { (request, response, data) in
+                guard let data = data else {
+                    failure?(TeamupError.unknown, nil)
+                    return
+                }
+                do {
+                    let membership = try self.decoder.decode(Membership.self, from: data)
+                    success?(membership)
+                } catch {
+                    failure?(error, nil)
+                }
+        }) { (request, response, error) in
+            failure?(error, response?.errorDetail)
+        }
     }
 }
